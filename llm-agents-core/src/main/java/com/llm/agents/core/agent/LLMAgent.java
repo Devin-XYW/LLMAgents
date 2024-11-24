baa44281feddd92e5e83223def165d0a17341cc9
Devin <wlmy52655@gmail.com>
2024-11-24 16:34:25 +0800

    NEW:Agent模块搭建1124

--- llm-agents-core/src/main/java/com/llm/agents/core/agent/LLMAgent.java
+++ llm-agents-core/src/main/java/com/llm/agents/core/agent/LLMAgent.java
@@ -1,5 +1,18 @@
 package com.llm.agents.core.agent;
 
+import com.llm.agents.core.chain.Chain;
+import com.llm.agents.core.llm.ChatOptions;
+import com.llm.agents.core.llm.LLM;
+import com.llm.agents.core.llm.response.AiMessageResponse;
+import com.llm.agents.core.message.ai.AiMessage;
+import com.llm.agents.core.prompt.TextPrompt;
+import com.llm.agents.core.prompt.template.TextPromptTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * @Author Devin
  * @Date 2024/11/24 16:21
@@ -7,6 +20,105 @@ package com.llm.agents.core.agent;
  **/
 public class LLMAgent extends Agent{
 
+    protected LLM llm;
+    protected ChatOptions chatOptions = ChatOptions.DEFAULT;
+    protected String prompt;
+    protected TextPromptTemplate promptTemplate;
 
+    public LLMAgent(){
 
+    }
+
+    public LLMAgent(LLM llm, String prompt) {
+        this.llm = llm;
+        this.prompt = prompt;
+        this.promptTemplate = new TextPromptTemplate(prompt);
+    }
+
+    public LLM getLlm() {
+        return llm;
+    }
+
+    public void setLlm(LLM llm) {
+        this.llm = llm;
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+        this.promptTemplate = new TextPromptTemplate(prompt);
+    }
+
+    public ChatOptions getChatOptions(){
+        return chatOptions;
+    }
+
+    public void setChatOptions(ChatOptions chatOptions){
+        if(chatOptions == null){
+            chatOptions = ChatOptions.DEFAULT;
+        }
+        this.chatOptions = chatOptions;
+    }
+
+    @Override
+    protected List<Parameter> defineInputParameter() {
+        if (this.promptTemplate == null) {
+            return null;
+        }
+
+        Set<String> keys = this.promptTemplate.getKeys();
+        if (keys == null || keys.isEmpty()) {
+            return null;
+        }
+
+        List<Parameter> parameters = new ArrayList<>(keys.size());
+        for (String key : keys) {
+            Parameter parameter = new Parameter(key, true);
+            parameters.add(parameter);
+        }
+        return parameters;
+    }
+
+    @Override
+    public Output execute(Map<String, Object> variables, Chain chain) {
+        TextPrompt textPrompt = promptTemplate.format(variables);
+        AiMessageResponse response = llm.chat(textPrompt,chatOptions);
+
+        if(chain != null){
+            chain.output(this,response);
+        }
+
+        return response.isError()
+                ? onError(response, chain)
+                : onMessage(response.getMessage());
+    }
+
+    protected Output onError(AiMessageResponse response, Chain chain) {
+        if (chain != null) {
+            chain.stopError(response.getErrorMessage());
+        }
+        return null;
+    }
+
+    protected Output onMessage(AiMessage aiMessage) {
+        List<OutputKey> outputKeys = getOutputKeys();
+        if (outputKeys != null && outputKeys.size() == 1) {
+            return Output.of(outputKeys.get(0), aiMessage.getContent());
+        }
+
+        return Output.ofDefault(aiMessage.getContent());
+    }
+
+    @Override
+    public String toString() {
+        return "LLMAgent{" +
+                "llm=" + llm +
+                ", prompt='" + prompt + '\'' +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
