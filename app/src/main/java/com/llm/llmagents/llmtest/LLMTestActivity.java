0a7e75295b7be225d08bab6494d0ceeb1af8ba21
Devin <wlmy52655@gmail.com>
2024-12-03 22:46:57 +0800

    New:spark大模型测试

--- app/src/main/java/com/llm/llmagents/llmtest/LLMTestActivity.java
+++ app/src/main/java/com/llm/llmagents/llmtest/LLMTestActivity.java
@@ -24,6 +24,8 @@ import com.llm.llmagents.R;
 import com.llm.llmagents.llmtest.function.WeatherUtil;
 import com.llm.qwen.QwenLLm;
 import com.llm.qwen.QwenLLmConfig;
+import com.llm.saprk.SparkLlm;
+import com.llm.saprk.SparkLlmConfig;
 
 public class LLMTestActivity extends AppCompatActivity {
     private final static String TAG = "LLMTestActivity";
@@ -63,18 +65,32 @@ public class LLMTestActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.sparkQuery).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                querySpark();
+            }
+        });
+
+        findViewById(R.id.sparkFunction).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                useFunctionSpark();
+            }
+        });
+
     }
 
-    private StreamResponseListener mQwenListener = new StreamResponseListener() {
+    private StreamResponseListener mListener = new StreamResponseListener() {
         @Override
         public void onStart(ChatContext context) {
-            Log.i(TAG,"QwenListener onStart");
+            Log.i(TAG,"Listener onStart");
         }
 
         @Override
         public void onMessage(ChatContext context, MessageResponse response) {
             AiMessage message = response.getMessage();
-            Log.i(TAG,"QwenListener onMessage message="+message);
+            Log.i(TAG,"Listener onMessage message="+message);
             String result = message.getFullContent();
             handler.post(new Runnable() {
                 @Override
@@ -86,12 +102,12 @@ public class LLMTestActivity extends AppCompatActivity {
 
         @Override
         public void onStop(ChatContext context) {
-            Log.i(TAG,"QwenListener onStop");
+            Log.i(TAG,"Listener onStop");
         }
 
         @Override
         public void onFailure(ChatContext context, Throwable throwable) {
-            Log.i(TAG,"QwenListener onFailure throwable="+throwable.getMessage());
+            Log.i(TAG,"Listener onFailure throwable="+throwable.getMessage());
             mResultView.post(new Runnable() {
                 @Override
                 public void run() {
@@ -110,7 +126,7 @@ public class LLMTestActivity extends AppCompatActivity {
         String query = mEditText.getText().toString();
         TextPrompt prompt = new TextPrompt(query);
 
-        llm.chatStream(prompt,mQwenListener);
+        llm.chatStream(prompt, mListener);
     }
 
     private void useFunctionQwen(){
@@ -121,6 +137,53 @@ public class LLMTestActivity extends AppCompatActivity {
                 config.setApiKey("sk-3b52d74fcbc94c9191311e0678a826af");
                 config.setModel("qwen-turbo");
                 LLM llm = new QwenLLm(config);
+
+                String query = mEditText.getText().toString();
+
+                FunctionPrompt prompt = new FunctionPrompt(query, WeatherUtil.class);
+                FunctionMessageResponse response = llm.chat( prompt);
+                Log.i(TAG,"useFunctionQwen response="+response);
+                Object result = response.getFunctionResult();
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (result != null){
+                            mResultView.setText(result.toString());
+                        }else {
+                            mResultView.setText("未查询到相关结果");
+                        }
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private void querySpark(){
+        SparkLlmConfig config = new SparkLlmConfig();
+        config.setAppId("b6b080da");
+        config.setApiKey("b0a8911be4f0da5973efc6c9990088fd");
+        config.setApiSecret("ZDM3OThlMTZiOGFmMGZlNGM3OTcyMDE5");
+
+        LLM llm = new SparkLlm(config);
+
+        String query = mEditText.getText().toString();
+        TextPrompt prompt = new TextPrompt(query);
+
+        llm.chatStream(prompt, mListener);
+
+    }
+
+    private void useFunctionSpark(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                SparkLlmConfig config = new SparkLlmConfig();
+                config.setAppId("b6b080da");
+                config.setApiKey("b0a8911be4f0da5973efc6c9990088fd");
+                config.setApiSecret("ZDM3OThlMTZiOGFmMGZlNGM3OTcyMDE5");
+
+                LLM llm = new SparkLlm(config);
 
                 String query = mEditText.getText().toString();
 
